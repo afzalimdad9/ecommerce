aa828577c88807473c5e7f618fdf7573ca94e591
afzalimdad9 <afzalimdad9@gmail.com>
2023-01-10 10:50:30 +0500

    first commit

---
+++ src/component/layout/About/About.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://localhost:3000/about";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/web-developerasd/image/upload/v1673318026/avatars/rrsyavf1367q1uiwl9f6.webp"
+              alt="Founder"
+            />
+            <Typography>Afzal Ali</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>This is a sample wesbite made by @afzalimdad9.</span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a href="#" target="blank">
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="#" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
